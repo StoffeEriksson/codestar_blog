2d0a9ae11ddb3334a8f02a9503b5b0422cbb5b71
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-06-06 13:51:05 +0200

    add Meta class to post model

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,13 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title}"
+
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
