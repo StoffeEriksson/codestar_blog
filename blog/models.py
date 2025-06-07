130ea9beab97b3ad4b7cad24da20977363a47410
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-06-07 12:29:36 +0200

    added fixtures to blog

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -19,10 +18,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
-
+    
     def __str__(self):
-        return f"The title of this post is {self.title}"
-
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -33,9 +31,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField()
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+        
